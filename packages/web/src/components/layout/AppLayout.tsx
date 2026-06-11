ff886c49dd7937578b39046d2648543d793b18bf
abilfida <addimasyqy@gmail.com>
2026-06-12 01:40:30 +0700

    fix: auto close selection mode on location change

--- packages/web/src/components/layout/AppLayout.tsx
+++ packages/web/src/components/layout/AppLayout.tsx
@@ -1,8 +1,18 @@
 import { Header } from './Header';
 import { Sidebar } from './Sidebar';
 import { MainContent } from './MainContent';
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import { useSelectionStore } from '../../stores/useSelectionStore';
 
 export const AppLayout = () => {
+  const location = useLocation();
+  const clearSelection = useSelectionStore((s) => s.clearSelection);
+
+  useEffect(() => {
+    clearSelection();
+  }, [location.pathname, location.search, clearSelection]);
+
   return (
     <div className="h-screen w-screen flex flex-col overflow-hidden bg-surface">
       <Header />
